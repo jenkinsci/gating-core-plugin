3c012690c35ca13d5d0a0bd60e6c68c943df6780
Oliver Gondža <ogondza@gmail.com>
2021-01-15 10:14:07 +0100

    Remove statuspage components

--- src/test/java/io/jenkins/plugins/gating/FreestyleGatingTest.java
+++ src/test/java/io/jenkins/plugins/gating/FreestyleGatingTest.java
@@ -25,10 +25,6 @@ import hudson.model.FreeStyleProject;
 import hudson.model.JobProperty;
 import hudson.model.Queue;
 import hudson.model.queue.CauseOfBlockage;
-import io.jenkins.plugins.gating.GatingMatrices;
-import io.jenkins.plugins.gating.ResourceBlockage;
-import io.jenkins.plugins.gating.ResourceRequirementProperty;
-import io.jenkins.plugins.gating.ResourceStatus;
 import org.junit.Rule;
 import org.junit.Test;
 import org.jvnet.hudson.test.JenkinsRule;
@@ -38,6 +34,7 @@ import java.io.IOException;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 
@@ -51,8 +48,8 @@ import static org.junit.Assert.assertTrue;
 
 public class FreestyleGatingTest {
 
-    public static final String RESOURCE_NAME = "statuspage/p1/C1/r1";
-    public static final String COMPONENT_NAME = "statuspage/p2/C3";
+    public static final String RES1 = "statuspage/pageA/my-resource";
+    public static final String RES2 = "statuspage/Page #3/The Resource";
 
     @Rule
     public final JenkinsRule j = new JenkinsRule();
@@ -61,13 +58,13 @@ public class FreestyleGatingTest {
     public void allDown() throws Exception {
 
         Map<String, ResourceStatus> status = new HashMap<>();
-        status.put(RESOURCE_NAME, MyStatus.BELLY_UP);
-        status.put(COMPONENT_NAME, MyStatus.DECENT);
+        status.put(RES1, MyStatus.BELLY_UP);
+        status.put(RES2, MyStatus.DECENT);
 
-        Queue.Item item = runJob(status, new ResourceRequirementProperty(Arrays.asList(RESOURCE_NAME, COMPONENT_NAME)));
+        Queue.Item item = runJob(status, new ResourceRequirementProperty(Arrays.asList(RES1, RES2)));
         CauseOfBlockage cob = item.getCauseOfBlockage();
         assertEquals(
-                String.format("Some resource are not available: %s is BELLY_UP, %s is DECENT", RESOURCE_NAME, COMPONENT_NAME),
+                String.format("Some resource are not available: %s is BELLY_UP, %s is DECENT", RES1, RES2),
                 cob.getShortDescription()
         );
     }
@@ -75,10 +72,10 @@ public class FreestyleGatingTest {
     @Test
     public void allUp() throws Exception {
         HashMap<String, ResourceStatus> status = new HashMap<>();
-        status.put(RESOURCE_NAME, MyStatus.OK);
-        status.put(COMPONENT_NAME, MyStatus.OK);
+        status.put(RES1, MyStatus.OK);
+        status.put(RES2, MyStatus.OK);
 
-        runJob(status, new ResourceRequirementProperty(Collections.singletonList(COMPONENT_NAME)));
+        runJob(status, new ResourceRequirementProperty(Collections.singletonList(RES2)));
         assertTrue(j.getInstance().getQueue().isEmpty());
     }
 
@@ -86,13 +83,13 @@ public class FreestyleGatingTest {
     public void someDown() throws Exception {
 
         Map<String, ResourceStatus> status = new HashMap<>();
-        status.put(RESOURCE_NAME, MyStatus.BELLY_UP);
-        status.put(COMPONENT_NAME, MyStatus.OK);
+        status.put(RES1, MyStatus.BELLY_UP);
+        status.put(RES2, MyStatus.OK);
 
-        Queue.Item item = runJob(status, new ResourceRequirementProperty(Arrays.asList(RESOURCE_NAME, COMPONENT_NAME)));
+        Queue.Item item = runJob(status, new ResourceRequirementProperty(Arrays.asList(RES1, RES2)));
         CauseOfBlockage cob = item.getCauseOfBlockage();
         assertEquals(
-                String.format("Some resource are not available: %s is BELLY_UP", RESOURCE_NAME),
+                String.format("Some resource are not available: %s is BELLY_UP", RES1),
                 cob.getShortDescription()
         );
     }
@@ -100,19 +97,19 @@ public class FreestyleGatingTest {
     @Test
     public void configRoundtrip() throws Exception {
         Map<String, ResourceStatus> status = new HashMap<>();
-        status.put(RESOURCE_NAME, MyStatus.OK);
-        status.put(COMPONENT_NAME, MyStatus.BELLY_UP);
+        status.put(RES1, MyStatus.OK);
+        status.put(RES2, MyStatus.BELLY_UP);
         setStatus(status);
 
         FreeStyleProject p = j.createFreeStyleProject();
         List<String> expected;
 
-        expected = Arrays.asList(RESOURCE_NAME, COMPONENT_NAME);
+        expected = Arrays.asList(RES2, RES1);
         p.addProperty(new ResourceRequirementProperty(expected));
         j.configRoundtrip(p);
         assertEquals(expected, p.getProperty(ResourceRequirementProperty.class).getResouces());
 
-        expected = Collections.singletonList(COMPONENT_NAME);
+        expected = Collections.singletonList(RES2);
         p.addProperty(new ResourceRequirementProperty(expected));
         j.configRoundtrip(p);
         assertEquals(expected, p.getProperty(ResourceRequirementProperty.class).getResouces());
@@ -126,7 +123,7 @@ public class FreestyleGatingTest {
     @Test
     public void ui() throws Exception {
         GatingMatrices gm = GatingMatrices.get();
-        gm.update("statuspage", GatingMatrices.Snapshot.with("statuspage/pageA/componentB/resourceC", MyStatus.OK));
+        gm.update("statuspage", GatingMatrices.Snapshot.with("statuspage/pageA/resourceC", MyStatus.OK));
         gm.update("cachet", GatingMatrices.Snapshot.with("cachet/resource1", MyStatus.DECENT));
         gm.update("zabbix", GatingMatrices.Snapshot
                 .with("zabbix/host1.exeample.com", MyStatus.BELLY_UP)
@@ -138,7 +135,7 @@ public class FreestyleGatingTest {
 
         assertThat(gating, containsString("zabbix/host1.exeample.comBELLY_UP"));
         assertThat(gating, containsString("zabbix/host2.exeample.comOK"));
-        assertThat(gating, containsString("statuspage/pageA/componentB/resourceCOK"));
+        assertThat(gating, containsString("statuspage/pageA/resourceCOK"));
         assertThat(gating, containsString("cachet/resource1DECENT"));
     }
 
